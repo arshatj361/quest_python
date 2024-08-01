60617e814c7e59755667fb7173864288bae6a92a
arshatj361 <arshatj361@gmail.com>
2024-08-01 21:36:04 +0530

    day2 assignments

--- Day 2/notes/classwork/no_of_digits2.py
+++
@@ -1,7 +0,0 @@
-#Alternative Program to count the number of digits in a number
-
-input_number = int(input('Enter a number to count number of digits in it: '))
-
-count_of_digits = len(str(input_number))
-
-print(f'Number of digits in {input_number} is {count_of_digits}')
