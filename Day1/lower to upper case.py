e61b64c8675a41bebb7433e72dd31c0b01513317
arshatj361 <arshatj361@gmail.com>
2024-08-01 11:42:48 +0530

    Day 1 programs

---
+++ Day1/lower to upper case.py
@@ -0,0 +1,17 @@
+#Write a program to convert lower case letter to upper case
+
+#ALGORITHM
+
+#enter the input
+#convert it into upper case by input.upper()
+#lowercase by input.lower()
+#Then print it
+
+
+input_string=input('enter the string in lower case  ')
+uppercase_string=input_string.upper()
+print('The uppercase of the given string is',uppercase_string)
+
+
+
+
