821b76d68782ffe540d1c7cacce6fbb734787665
arshatj361 <arshatj361@gmail.com>
2024-08-02 07:12:42 +0530

    day3 assignment

--- Day 3/Day3_assignments/largest_smallest_list.py
+++ Day 3/Day3_assignments/largest_smallest_list.py
@@ -1,30 +1,20 @@
-def main():
-    
-    N = int(input("Enter the number of elements: "))
-    
-    
-    numbers = []
-    
-    
-    for i in range(N):
-        num = int(input(f"Enter number: "))
-        numbers.append(num)
-    
-    
-    if numbers:
-        
-        smallest = numbers[0]
-        largest = numbers[0]
-        
-        
-        for num in numbers:
-            if num < smallest:
-                smallest = num
-            if num > largest:
-                largest = num
-        
-        
-        print("Smallest element:", smallest)
-        print("Largest element:", largest)
-    else:
-        print("The list is empty!")
+# Program to find the smallest and largest number in a list
+
+def largest_number(list):
+    largest_number=max(list)
+    return largest_number
+
+def smallest_number(list):
+    smallest_number=min(list)
+    return smallest_number
+
+N = int(input("Enter the number of elements: "))
+list = []
+
+for i in range(N):
+    number = int(input("Enter the numbers in the list: ")) 
+    list.append(number)
+
+print("the largest number is", largest_number(list))  
+print("the smallest number is", smallest_number(list))
+
