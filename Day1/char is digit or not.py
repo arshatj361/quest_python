e1a55d24fb36096e29c0809331131734da783861
arshatj361 <arshatj361@gmail.com>
2024-08-04 12:11:54 +0530

    day 4 notes

--- Day1/char is digit or not.py
+++
@@ -1,8 +0,0 @@
-#check if the given character is a digit or not
-
-print('Enter a character to check if it is a digit')
-ch=input()
-if ch>='0' and ch<='9':
-    print('given character is a digit')
-else:
-    print('given character is not a digit')
